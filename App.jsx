b65fba8d5ae44f489f686e68c64b2be28b8577a5
Roberto Caballero <94751976+robertocaba@users.noreply.github.com>
2024-03-11 10:52:39 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css';
+import SoapValencia from './SoapExample/Soap';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> SoapExample </p>
+        <>
+          <SoapValencia/>
+          
+        </>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
